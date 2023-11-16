f830e4e2378b92d345d50781cb18b8dbf985c0a2
stevensotelo <stevenbetancurt@hotmail.com>
2023-11-16 09:44:52 -0500

    added new methods to historical

--- R/historical.R
+++ R/historical.R
@@ -10,8 +10,8 @@
 #' @examples
 #' url_root = "https://webapi.aclimate.org/api/"
 #' stations=c("58504f1a006cb93ed40eebe2","58504f1a006cb93ed40eebe3")
-#' obj_f = get_historical_climatology(url_root, stations)
-#' print(obj_f)
+#' df = get_historical_climatology(url_root, stations)
+#' print(df)
 #'
 #' @export
 get_historical_climatology = function(url_root, stations){
@@ -49,8 +49,8 @@ get_historical_climatology = function(url_root, stations){
 #' @examples
 #' url_root = "https://webapi.aclimate.org/api/"
 #' stations=c("58504f1a006cb93ed40eebe2","58504f1a006cb93ed40eebe3")
-#' obj_f = get_historical_historicalclimatic(url_root, stations)
-#' print(obj_f)
+#' df = get_historical_historicalclimatic(url_root, stations)
+#' print(df)
 #'
 #' @export
 get_historical_historicalclimatic = function(url_root, stations){
@@ -74,5 +74,83 @@ get_historical_historicalclimatic = function(url_root, stations){
                         }))
                     }))
                 }))
+    return (df)
+}
+
+#' Get years with historical crop performance data
+#'
+#' @description  Through this endpoint you can obtain the years that contain historical crop performance data of the selected weather station.
+#'
+#' @param url_root Url root where the API is located.
+#' @param stations Array of strings with the ids of the weather stations.
+#'
+#' @return A data.frame, with the climatology of different climatic variables
+#'
+#' @examples
+#' url_root = "https://webapi.aclimate.org/api/"
+#' stations=c("58504f1a006cb93ed40eebe2","58504f1a006cb93ed40eebe3")
+#' df = get_historical_historicalyieldyears(url_root, stations)
+#' print(df)
+#'
+#' @export
+get_historical_historicalyieldyears = function(url_root, stations){
+    library(httr)
+    library(rjson)
+    httr::set_config(config(ssl_verifypeer = 0L))
+    # Download data
+    ws = paste(stations,collapse=",")
+    url = paste0(url_root,"Historical/HistoricalYieldYears/",ws,"/json")
+    request = GET(url)
+    response = content(request, as = "text", encoding = "UTF-8")
+    data = fromJSON(response)
+    #df = #do.call(rbind,
+                #lapply(data,function(y){
+                    #data.frame(year=data)
+                #}))
+    df = data.frame(year=data)
+    return (df)
+}
+
+#' Get historical crop performance
+#'
+#' @description Access yield data obtained through the crop model process using the HTTP GET method. This endpoint provides valuable information on crop yields, offering insights derived from the crop model process.
+#'
+#' @param url_root Url root where the API is located.
+#' @param stations Array of strings with the ids of the weather stations.
+#'
+#' @return A data.frame, with the crop forecast result for the weather stations.
+#'
+#' @examples
+#' url_root = "https://webapi.aclimate.org/api/"
+#' stations=c("58504f1a006cb93ed40eebe2","58504f1a006cb93ed40eebe3")
+#' df = get_forecast_crop(url_root, stations)
+#' print(head(df))
+#'
+#' @export
+get_historical_historicalyield = function(url_root, stations){
+    library(httr)
+    library(rjson)
+    httr::set_config(config(ssl_verifypeer = 0L)) 
+    # Download data
+    ws = paste(stations,collapse=",")
+    url = paste0(url_root,"Historical/HistoricalYield/",ws,"/json")
+    request = GET(url)
+    response = content(request, as = "text", encoding = "UTF-8")
+    data = fromJSON(response)
+    df = do.call(rbind,
+                    lapply(data$yield,function(w){
+                        do.call(rbind,lapply(w$yield,function(wy){
+                            do.call(rbind,lapply(wy$data,function(y){
+                                data.frame(ws_id=w$weather_station,
+                                            cultivar=wy$cultivar, soil=wy$soil, start=wy$start, end=wy$end,
+                                            measure=y$measure,
+                                            median=y$median,avg=y$avg,min=y$min,max=y$max,
+                                            quar_1=y$quar_1,quar_2=y$quar_2,quar_3=y$quar_3,
+                                            conf_lower=y$conf_lower,conf_upper=y$conf_upper,
+                                            sd=y$sd,perc_5=y$perc_5,perc_95=y$perc_95,
+                                            coef_var=y$coef_var)
+                            }))
+                        }))
+                    }))
     return (df)
 }
